964f4cd045d14cc54977fddc173f8c7fbd255719
v0xtex <148804206+v0xtex@users.noreply.github.com>
2024-02-11 17:17:15 +0100

    Update Genre.jsx

--- src/components/Genre.jsx
+++ src/components/Genre.jsx
@@ -25,7 +25,7 @@ function Genre() {
     return (
         <>
         <Helmet>
-            <title>BlueBird Movies | Genres</title>
+            <title>Tigex Movies | Genres</title>
         </Helmet>
 
         <div className='flex flex-wrap justify-center px-2'>
