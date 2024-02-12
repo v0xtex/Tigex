c164cf0d3f0c929b7564c13cf23a92dca55fec4f
v0xtex <148804206+v0xtex@users.noreply.github.com>
2024-02-12 11:44:41 +0100

    Update Container.jsx

--- src/pages/Container.jsx
+++ src/pages/Container.jsx
@@ -1,20 +1,26 @@
-import React,{useEffect, useContext} from "react";
+import React, { useEffect, useContext, useState } from "react";
 import Contextpage from '../Contextpage'
 import Movies from "../components/Movies";
 import Searchbar from "../components/Searchbar";
 import { useParams } from 'react-router-dom'
 import Search from "../pages/Search"
 
-
 function Container() {
-    const { setMovies } = useContext(Contextpage);
-    const { query } = useParams()
-    return (
-        <section>
-        <Searchbar />
-        {query ? <Search query={query} /> : <Movies />}
-        </section>
-    )
+  const { setMovies } = useContext(Contextpage);
+  const { query } = useParams();
+  const [visits, setVisits] = useState(0);
+
+  useEffect(() => {
+    setVisits(visits + 1);
+  }, []);
+
+  return (
+    <section>
+      <Searchbar />
+      {query ? <Search query={query} /> : <Movies />}
+      <p>Nombre de visites : {visits}</p>
+    </section>
+  )
 }
 
 export default Container;
