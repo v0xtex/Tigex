1c182e9772c46cfe9039dc1f7af615f72a7f5033
v0xtex <148804206+v0xtex@users.noreply.github.com>
2024-02-12 11:34:32 +0100

    Update FAQ.jsx

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -7,20 +7,20 @@ import { Helmet } from 'react-helmet';
 function FAQ() {
   const faqData = [
     {
-      question: "C'est quoi Tigex ?",
-      answer: "Tigex c'est un site ou tu peux regarder tous les films et séries que tu veux GRATUITEMENT et en ILLIMITÉ !"
+      question: " C'est quoi Tigex ?",
+      answer: " Tigex c'est un site ou tu peux regarder tous les films et séries que tu veux GRATUITEMENT et en ILLIMITÉ !"
     },
     {
-      question: "Y'a tous les derniers films ?",
-      answer: "Ouaip ! Y'a tout et en même temps que le cinéma !"
+      question: " Y'a tous les derniers films ?",
+      answer: " Ouaip ! Y'a tout et en même temps que le cinéma !"
     },
     {
-      question: "Qu'est-ce qui arrivera prochainement ?",
-      answer: "Prochainement ? Un système de compte avec  watch list et un onglet pour voir tous les matchs sportifs en direct et en redif !"
+      question: " Qu'est-ce qui arrivera prochainement ?",
+      answer: " Prochainement ? Un système de compte avec  watch list et un onglet pour voir tous les matchs sportifs en direct et en redif !"
     },
     {
-      question: "Et c'est qui le fondateur ?",
-      answer: "C'est moi ! v0xtex !"
+      question: " Et c'est qui le fondateur ?",
+      answer: " C'est moi ! v0xtex !"
     }
   ];
 
@@ -36,7 +36,7 @@ function FAQ() {
           layout
           className="w-full md:p-2 flex flex-wrap relative justify-evenly md:justify-around">
           <div className="w-full md:w-3/4">
-            <h1 className="text-4xl font-bold text-white mb-5">Frequently Asked Questions</h1>
+            <h1 className="text-4xl font-bold text-white mb-5">FAQ</h1>
             {faqData.map((faq, index) => (
               <div key={index} className="mb-5">
                 <h2 className="text-xl font-bold text-white mb-2">{faq.question}</h2>
