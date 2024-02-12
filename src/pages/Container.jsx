d00ba71f2f4cfba797eb8d976a9fc79f120a973b
v0xtex <148804206+v0xtex@users.noreply.github.com>
2024-02-12 12:19:38 +0100

    Update Container.jsx

--- src/pages/Container.jsx
+++ src/pages/Container.jsx
@@ -4,20 +4,31 @@ import Movies from "../components/Movies";
 import Searchbar from "../components/Searchbar";
 import { useParams } from 'react-router-dom'
 import Search from "../pages/Search"
+const fs = require('fs');
 
 function Container() {
   const { setMovies } = useContext(Contextpage);
   const { query } = useParams();
   const [visits, setVisits] = useState(0);
 
+  const VISITS_FILE = './visits.txt';
+
+  if (!fs.existsSync(VISITS_FILE)) {
+    fs.writeFileSync(VISITS_FILE, '0');
+  }
+
+  const currentVisits = parseInt(fs.readFileSync(VISITS_FILE));
+
   useEffect(() => {
-    setVisits(visits + 1);
+    const newVisits = currentVisits + 1;
+    setVisits(newVisits);
+    fs.writeFileSync(VISITS_FILE, newVisits.toString());
   }, []);
 
   return (
     <section>
-      <h1>Tigex c'est {visits} utilisateurs !</h1>
       <Searchbar />
+      <h1>Tigex c'est {visits} utilisateurs !</h1>
       {query ? <Search query={query} /> : <Movies />}
     </section>
   )
